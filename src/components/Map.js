fc0b375606305b07bf49e13f2e361e602c86e21c
AnasShar <anas10895@gmail.com>
2020-11-28 20:59:14 +0300

    map is working, wont show user location

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import {Text, StyleSheet} from 'react-native'
-
+import MapView from 'react-native-maps'
 const Map = () => {
     return (
-        <Text>Im a Map</Text>
+        <MapView style={styles.map} 
+            initialRegion={{
+                latitude: 21.515231,
+                longitude: 39.227759,
+                latitudeDelta: 0.01 ,
+                longitudeDelta:0.01
+            }}
+        />
 
     )
 }
 const styles = StyleSheet.create({
+    map:{
+        height:300,
+    }
 })
 export default Map;
